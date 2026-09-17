6d610467659ad1b1595906e78e07b4529600bde7
marceleow <marceleo.pputra@gmail.com>
2026-09-17 15:30:31 +0800

    chore: initialize: api, admin, and mobile apps

---
+++ apps/admin/vite.config.ts
@@ -0,0 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+import adapter from "@sveltejs/adapter-static";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import { lazyPlugins } from "vite-plus";
+
+export default defineConfig({
+  plugins: lazyPlugins(() => [
+    tailwindcss(),
+    sveltekit({
+      compilerOptions: {
+        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+        runes: ({ filename }) =>
+          filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
+      },
+      adapter: adapter(),
+    }),
+  ]),
+});
